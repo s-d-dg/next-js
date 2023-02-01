72e2e71cbd57cdd77c899c8ea69888c1b7fc446c
Damian Gąsiorowski <softwaredevelopment.dg@gmail.com>
2023-02-01 18:35:39 +0100

    [My-Blog] - added Hero component

---
+++ src/components/home-page/hero.js
@@ -0,0 +1,22 @@
+import classes from "./hero.module.css";
+import Image from "next/image";
+
+export default function Hero() {
+  return (
+    <section class={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/damian.jpg"
+          alt="An image showing Damian"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi I'm Damian</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
+import Hero from '../components/home-page/hero';
 
 export default function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      {/* <FeaturedPosts /> */}
     </Fragment>
   );
 }
